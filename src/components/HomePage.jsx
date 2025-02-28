517ebd0f01a66e2c5c5d93bc2db52a5bed9166de
AliHusnain5 <ah4008189@gmail.com>
2025-02-28 10:38:47 +0500

    new work

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-    <h1>ali husnian</h1>  
+    <h1>ali husnian</h1> 
+    <h2>m azam bangali </h2> 
+    <h1>jamil bangali</h1>
+    <h2>zain sab</h2>
     </div>
   )
 }
